02f3bc5b92fcc40a8cdeedebe478cca0d5508ef7
tmaraki <toshiemaraki@gmail.com>
2024-03-10 15:48:38 -0400

    created auth.js file under utils folder in order to connect to blogPostRoutes file. Added code to make blogPostRoutes functional

---
+++ controllers/api/blogPostRoutes.js
@@ -0,0 +1,35 @@
+const router = require ('express').Router();
+const { BlogPost } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.post('/', withAuth, async (req, res) => {
+    try {
+        const newBlogPost = await BlogPost.create({
+            ...req.body,
+            user_id: req.session.user_id,
+        });
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const blogPostData = await BlogPost.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id
+            },
+        });
+        
+        if (!blogPostData) {
+            res.status(400).json({message: 'no blogpost found with this id'});
+            return;
+        }
+        res.status(200).json(blogPostData);
+    } catch (error) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
